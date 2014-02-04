c60a4677d80a0ff21547f64ba296553d340b4bd8
Thomas DE BONA <thomas.debona@gmail.com>
2014-02-04 18:08:30 +0100

    Validate the presence of title and url

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Bookmark do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Bookmark.new(title: 'dummy title', url:'http://www.dummy.com') }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:url) }
+  it { should be_valid }
+
+  context 'when title is not present' do
+    before { subject.title = ' ' }
+    it { should_not be_valid }
+  end
+
+  context 'when url is not present' do
+    before { subject.url = ' ' }
+    it { should_not be_valid }
+  end
+
+
 end
