018b981e7e7f2137130dc2630d90075046097698
Thomas DE BONA <thomas.debona@gmail.com>
2014-02-04 17:57:59 +0100

    Require presence and uniqueness of `name`

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,3 @@
 class Tag < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Tag.new(name: 'dummy name') }
+
+  it { should respond_to(:name) }
+  it { should be_valid }
+
+  context 'when name is not present' do
+    before { subject.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  context 'when name is already taken' do
+    before do
+      tag_with_same_name = subject.dup
+      tag_with_same_name.name = subject.name.upcase
+      tag_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
 end
